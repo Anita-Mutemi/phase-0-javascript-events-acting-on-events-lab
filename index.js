740dbc9e8c7e97ee007c74b1e669d809f2e82d69
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-06-16 11:55:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const leftEdge = parseInt(dodger.style.left, 10);
+    if (leftEdge > 0) {
+      dodger.style.left = `${leftEdge - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const leftEdge = parseInt(dodger.style.left, 10);
+    const rightEdge = leftEdge + dodger.offsetWidth;
+  
+    if (rightEdge < window.innerWidth) {
+      dodger.style.left = `${leftEdge + 1}px`;
+    }
+  }
+  
+  
+  
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (event.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+  
