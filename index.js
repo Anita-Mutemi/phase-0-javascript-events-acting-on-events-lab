20b1ceaf3934ce83ad339827c22127e6abc19e00
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-07-12 02:42:52 +0300

    edited index.js according to test

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.background = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+
 function moveDodgerLeft() {
-    const dodger = document.getElementById('dodger');
-    const leftEdge = parseInt(dodger.style.left, 10);
-    if (leftEdge > 0) {
-      dodger.style.left = `${leftEdge - 1}px`;
-    }
+  const leftEdge = parseInt(dodger.style.left, 10);
+  if (leftEdge > 0) {
+    dodger.style.left = `${leftEdge - 1}px`;
   }
-  
-  function moveDodgerRight() {
-    const dodger = document.getElementById('dodger');
-    const leftEdge = parseInt(dodger.style.left, 10);
-    const rightEdge = leftEdge + dodger.offsetWidth;
-  
-    if (rightEdge < window.innerWidth) {
-      dodger.style.left = `${leftEdge + 1}px`;
-    }
+}
+
+function moveDodgerRight() {
+  const leftEdge = parseInt(dodger.style.left, 10);
+  const rightEdge = leftEdge + dodger.offsetWidth;
+
+  if (rightEdge < window.innerWidth) {
+    dodger.style.left = `${leftEdge + 1}px`;
   }
-  
-  
-  
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  dodger = document.getElementById('dodger');
+
   document.addEventListener('keydown', function(event) {
     if (event.key === 'ArrowLeft') {
       moveDodgerLeft();
@@ -26,4 +30,4 @@ function moveDodgerLeft() {
       moveDodgerRight();
     }
   });
-  
+});
